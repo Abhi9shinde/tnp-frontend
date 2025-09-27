560e9d56c428f107af123b68020c1bc79b2d296f
Ram Gavhane <ramgavhane28@gmail.com>
2025-09-28 00:14:55 +0530

    test

--- app/(main)/profile/page.tsx
+++ app/(main)/profile/page.tsx
@@ -7,7 +7,7 @@ type Props = {}
 const Profile = (props: Props) => {
   return (
     <div className='flex flex-col items-center justify-center h-screen bg-white'>
-        <BackgroundRippleEffect />
+        
         <div className="relative z-10">
           <SignupFormDemo/>
         </div>

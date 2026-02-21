935912a4c97a7ef456592c7e643be662a905af54
Pranav Turkar <pranavturkar93@gmail.com>
2026-02-21 15:02:32 +0530

    export job added

--- hooks/useStudentNotification.ts
+++ hooks/useStudentNotification.ts
@@ -1,13 +1,20 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
+export type Job = {
+  id: string;
+  company: string;
+  role: string;
+  createdAt: string;
+  deadline: string;
+};
+
 export function useStudentNotifications() {
-  return useQuery({
+  return useQuery<Job[]>({
     queryKey: ["student-notifications"],
     queryFn: async () => {
       const { data } = await axios.get(
-        "/api/my-proxy/api/v1/student/notifications",
-        { withCredentials: true }
+        "/api/my-proxy/api/v1/student/notifications"
       );
       return data.jobs;
     },
